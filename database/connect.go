a282713d722e7f73883cb1cebac78d3bc532a29b
K0sik-cH1rVa <timonchaves7@gmail.com>
2026-05-19 16:22:54 +0300

    2code

--- database/connect.go
+++ database/connect.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,7 +12,7 @@ import (
 func Connect(ctx context.Context) (*pgxpool.Pool, error) {
 	dbURL := os.Getenv("DbURL")
 	if dbURL == "" {
-		dbURL = "postgres://postgres:22853@localhost:5432/postgres" // Значение по умолчанию
+		fmt.Println("Пустой пароль")
 	}
 	return pgxpool.New(ctx, dbURL)
 }
